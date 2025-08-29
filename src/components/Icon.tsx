2db91b4ca5aa1527829d4956f35df79ab8c1f110
Norddin Oubahman <n.oubahman@hotmail.com>
2025-08-29 12:29:51 +0200

    Install and setup react-native-vector icons and create a reusable icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,15 @@
+// import { View } from 'react-native';
+import React from 'react';
+import { AntDesign } from '@react-native-vector-icons/ant-design';
+import { IconName } from '../models/icon';
+import THEME from '../constants/theme';
+
+type Props = {
+  name: IconName;
+  size?: number;
+};
+const Icon = ({ name, size = 30 }: Props) => {
+  return <AntDesign name={name} size={size} color={THEME.COLORS.primary} />;
+};
+
+export default Icon;
---
+++ src/models/icon.ts
@@ -0,0 +1,4 @@
+import AntDesign from '@react-native-vector-icons/ant-design';
+import { ComponentProps } from 'react';
+
+export type IconName = ComponentProps<typeof AntDesign>['name'];
